bd9f02cbf935011958bc07795eafbc6015ce2e48
maxime <maximebatista18@gmail.com>
2024-07-04 10:32:29 +0200

    --amend

--- model/detector.py
+++ model/detector.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 
 # model = YOLO("../../coc-base-generator/runs/detect/train9/weights/best.pt")
-model = YOLO("runs/detect/train7/weights/best.pt")
+model = YOLO("runs/detect/train32/weights/best.pt")
 
 # results = model("datasets/buildings/images/train/Archer_Tower1.png")
-results = model("test_images/img_1.png")
+# results = model("../../coc-base-generator/out/images/test/village_0.png")
+results = model("test_images/th11_defence_97.jpg")
 
 for result in results:
     # Display or save each result
--- model/train.py
+++ model/train.py
@@ -1,14 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train5/weights/best.pt")
+model = YOLO("runs/detect/train41/weights/best.pt")
+# model = YOLO("yolov8n.pt")
 
-IMAGE_SIZE = 2000
+IMAGE_SIZE = 640
 
 model.train(
     data="../../coc-base-generator/out/data.yaml",
     epochs=100000,
     batch=2,
     imgsz=IMAGE_SIZE,
-    augment=True,
+    augment=False,
 )
 
